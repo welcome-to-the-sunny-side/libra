cede10ab9a3c8d700010f97b7afe27e88bb7b843
shreyaswans <shreyaswans@gmail.com>
2024-09-08 12:45:56 +0530

    updated something

--- string/KMP.cpp
+++ string/KMP.cpp
@@ -1,6 +1,7 @@
 template <typename T>
-vector<int> kmp_table(int n, const T &s)
+vector<int> kmp_table(const T &s)
 {
+    int n = s.size();
     vector<int> p(n, 0);
     int k = 0;
     for (int i = 1; i < n; i ++)
@@ -14,19 +15,15 @@ vector<int> kmp_table(int n, const T &s)
     return p;
 }
 
+// returns 0-indexed positions of occurrences of s in w, p is kmp-table of s
 template <typename T>
-vector<int> kmp_table(const T &s)
+vector<int> kmp_search(const T &s, const T &w, const vector<int> &p)
 {
-    return kmp_table((int)s.size(), s);
-}
-
-template <typename T>
-vector<int> kmp_search(int n, const T &s, int m, const T &w, const vector<int> &p)
-{
+    int n = s.size(), m = w.size();
     assert(n >= 1 && (int)p.size() == n);
     vector<int> res;
     int k = 0;
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m; i ++)
     {
         while (k > 0 && (k == n || !(w[i] == s[k])))
             k = p[k - 1];
@@ -36,11 +33,4 @@ vector<int> kmp_search(int n, const T &s, int m, const T &w, const vector<int> &
             res.push_back(i - n + 1);
     }
     return res;
-    // returns 0-indexed positions of occurrences of s in w
-}
-
-template <typename T>
-vector<int> kmp_search(const T &s, const T &w, const vector<int> &p)
-{
-    return kmp_search((int)s.size(), s, (int)w.size(), w, p);
 }
