ad402c4969ff0e7b2d0eca777bb4f6e721235d40
shreyaswans <shreyaswans@gmail.com>
2024-09-10 17:23:43 +0530

    updated something

--- data/MonotonicQueue.cpp
+++ data/MonotonicQueue.cpp
@@ -1,28 +1,39 @@
+class Info
+{
+public:
+    int x;
+    Info() : x(0) {};
+    Info(int v) : x(v) {};
+
+    bool opt(const Info &o) const
+    {
+        return x > o.x;
+    }
+};
 template<typename T>
 class MonoQ
 {
 public:
-    static constexpr int neutral = 0;
+    T neutral;
 
-    deque<pair<T, T>> q;
-    function<bool(const T&, constT&)> opt;
+    deque<pair<T, int>> q;
     int l, r;
 
-    MonoQ(function<bool(const T&, constT&)> cmp) : l(0), r(0), opt(cmp) {};
+    MonoQ() : l(0), r(0) {};
 
     int Size()
     {
         return r - l;
     }
-    int Opt()
+    T Opt()
     {
-        return (Size() ? q.front().first : neutral);
+        return (Size() ? q.front().first : T());
     }
     void Push(T x)
     {
-        while(!q.empty() and opt(x, q.back().first))
+        while(!q.empty() and x.opt(q.back().first))
             q.pop_back();
-        q.push_back({T, r ++});
+        q.push_back({x, r ++});
     }
     void Pop()
     {
