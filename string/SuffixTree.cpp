b98bb7ac17a31cc1d55648d6552bdf4cda6a0881
shreyaswans <shreyaswans@gmail.com>
2024-09-12 01:00:05 +0530

    updated something

--- string/SuffixTree.cpp
+++ string/SuffixTree.cpp
@@ -1,35 +1,27 @@
 class SuffixTree
 {
 public:
-    int n;
+    int n, m;
     vector<int> sa, lcp, dep, rep;
     vector<vector<int>> adj;
 
-    SuffixTree(int n, vector<int> sa, vector<int> lcp) : n(n), sa(sa), lcp(lcp), dep(2 * n), rep(2 * n), adj(2 * n) 
+    SuffixTree(int n, vector<int> sa, vector<int> lcp) : n(n), m(n - 1), sa(sa), lcp(lcp), dep(2 * n + 1), rep(2 * n + 1), adj(2 * n + 1) 
     {
-        assert((int)sa.size() == n and (int)lcp.size() == n - 1);
-
         vector<int> l(n), r(n), p(n);
         for(int i = 0; i < n; i ++) 
-            l[i] = r[i] = p[i] = i, dep[i] = n - i, rep[i] = i;
-        
+            l[i] = r[i] = i, p[i] = sa[i], dep[i] = n - i, rep[i] = i;
         vector<vector<int>> q(n);
         for(int i = 0; i < n - 1; i ++)
             q[lcp[i]].push_back(i);
-        
-        for(int d = n - 1, x = n - 1; d >= 0; d --)
+        for(int d = n - 1; d >= 0; d --)
             for(auto i : q[d])
             {
-                int u = ++ x;
+                int u = ++ m;
                 dep[u] = d;
-
-                int v1 = p[i], v2 = p[i + 1];
-                adj[u].push_back(v1), adj[u].push_back(v2);
-
-                l[r[i + 1]] = l[i];
-                r[l[i]] = r[i + 1];
-                p[l[i]] = p[r[i]] = u;
-                rep[u] = rep[v1];
+                adj[u] = {p[i], p[i + 1]};
+                rep[u] = rep[p[i]];
+                l[r[i + 1]] = l[i], r[l[i]] = r[i + 1];
+                p[l[i]] = p[r[i + 1]] = u;
             }
     }
 };
