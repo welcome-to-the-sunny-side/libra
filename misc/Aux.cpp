0e1c502bd775ac6309a6d2d00981c5e63cb2cb3e
shreyaswans <shreyaswans@gmail.com>
2024-09-11 19:32:20 +0530

    updated something

--- misc/Aux.cpp
+++ misc/Aux.cpp
@@ -1,4 +1,21 @@
 namespace aux
 {
-
-};
+    namespace bitwise
+    {
+        int64_t p(int i)
+        {
+            return 1LL << i;
+        }
+    }
+    namespace math
+    {
+        int ceil_div(int x, int y)
+        {
+            return (x + y - 1)/y;
+        }
+        int64_t ceil_div(int64_t x, int64_t y)
+        {
+            return (x + y - 1)/y;
+        }
+    }
+}
