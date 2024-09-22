356a74b0e4c4595919639d157c6c5f10fae6f67f
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2024-09-23 02:25:26 +0530

    updated something

---
+++ graph/tree/VirtualTree.cpp
@@ -0,0 +1,44 @@
+template <typename L>
+vector<pair<int, int>> VirtualTree(vector<int> key, const vector<int> &tin, const vector<int> &tout, const L &lca)
+{
+    assert(!key.empty());
+
+    sort(key.begin(), key.end(), [&](int a, int b)  
+        { return tin[a] < tin[b]; });
+
+    vector<int> l;
+    for (int i = 0; i < key.size() - 1; i++)
+        l.push_back(lca.lca(key[i], key[i + 1]));
+    
+    for (auto p : l)
+        key.push_back(p);
+
+    sort(key.begin(), key.end(), [&](int a, int b)
+         { return tin[a] < tin[b]; });
+    
+    key.erase(unique(key.begin(), key.end()), key.end());
+    reverse(key.begin(), key.end());
+
+    stack<int> stk;
+    vector<pair<int, int>> edge;
+
+    // stk contains path from root to current node, key contains unvisited nodes
+
+    stk.push(key.back());
+    key.pop_back();
+
+    while (!stk.empty())
+    {
+        int u = stk.top();
+        if (!key.empty() and tin[u] <= tin[key.back()] and tout[key.back()] <= tout[u])
+        {
+            edge.push_back(make_pair(u, key.back()));
+            stk.push(key.back());
+            key.pop_back();
+            continue;
+        }
+        stk.pop();
+    }
+
+    return edge;
+};
