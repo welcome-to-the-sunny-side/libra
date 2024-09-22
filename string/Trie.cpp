1abc32168b49486e57c72c272b14021e0a379dac
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2024-09-23 02:57:21 +0530

    updated something

--- string/Trie.cpp
+++ string/Trie.cpp
@@ -5,23 +5,23 @@ public:
     map<int, int> next;
     TrieNode() : leaf(0) {};
 };
-template<typename S, typename T>
+template<typename S, typename T, typename C>
 class Trie
 {
 public:
-    int n;
     int r = 0, p = 0;
-    S base;
     vector<T> t;
+    C c;
 
-    Trie(int n, S base) : t(n), base(base) {};
+    Trie(int n, C c) : t(n), c(c) {};
 
     void Insert(const vector<S> &s)
     {
         int v = r;
         for(auto x : s)
         {
-            int f = x - base;
+            int f = c(x);
+        
             if(t[v].next[f] == 0)
                 t[v].next[f] = ++ p;
             v = t[v].next[f];
