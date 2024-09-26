934920198e1e610a9abd2c306005bb8ebea7ac4c
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2024-09-26 21:22:06 +0530

    updated something

--- data/Bitset.cpp
+++ data/Bitset.cpp
@@ -30,14 +30,14 @@ public:
     Bitset operator & (const Bitset &other)
     {
         Bitset result(max(n, other.n), false);
-        for(int i = 0; i < min(m, other.m); i ++)
+        for(size_t i = 0; i < min(m, other.m); i ++)
             result.b[i] = b[i] & other.b[i];
         return result;
     }
 
     void operator &= (const Bitset &other)
     {
-        for(int i = 0; i < min(m, other.m); i ++)
+        for(size_t i = 0; i < min(m, other.m); i ++)
             b[i] &= other.b[i];
         if(m > other.m)
             fill(b.begin() + other.m, b.begin() + m, T(0));
@@ -47,21 +47,21 @@ public:
     {
         Bitset result((n > other.n ? *this : other));
         const Bitset* overlap = (m > other.m ? &other : this);
-        for(int i = 0; i < min(m, other.m); i ++)
+        for(size_t i = 0; i < min(m, other.m); i ++)
             result.b[i] |= overlap->b[i];
         return result;
     }
 
     void operator |= (const Bitset &other)
     {
-        for(int i = 0; i < min(m, other.m); i ++)
+        for(size_t i = 0; i < min(m, other.m); i ++)
             b[i] |= other.b[i];
     }
 
     Bitset operator << (int x)
     {
         if(x == 0)
-            return Bitset(*this);
+            return *this;
 
         Bitset result(n);
 
@@ -75,7 +75,7 @@ public:
         if(d > 0)
         {
             result.b[m - 1] <<= d;
-            for(int i = m - 2; i >= 0; i --)
+            for(size_t i = m - 2; i >= 0; i --)
             {
                 result.b[i + 1] |= (result.b[i] >> (B - d));
                 result.b[i] <<= d;
@@ -88,7 +88,7 @@ public:
     Bitset operator >> (int x)
     {
         if(x == 0)
-            return Bitset(*this);
+            return *this;
 
         Bitset result(n);
 
@@ -102,13 +102,41 @@ public:
         if(d > 0)
         {
             result.b[0] >>= d;
-            for(int i = 1; i < m; i ++)
+            for(size_t i = 1; i < m; i ++)
             {
                 result.b[i - 1] |= (result.b[i] << (B - d));
                 result.b[i] >>= d;
             }
         }
         return result;
+    }
+
+    void operator >>= (int x)
+    {
+        if(x == 0)
+            return;
+
+        if(x >= n)
+        {
+            fill(b.begin(), b.end(), T(0));
+            return;
+        }
+
+        int s = x/B, d = x % B;
+
+        if(s > 0)
+            for(size_t i = 0; i < m - s; i ++)
+                b[i] = b[i + s];
+
+        if(d > 0)
+        {
+            b[0] >>= d;
+            for(size_t i = 1; i < m; i ++)
+            {
+                b[i - 1] |= (b[i] << (B - d));
+                b[i] >>= d;
+            }
+        }
     }
 
     void Show()
