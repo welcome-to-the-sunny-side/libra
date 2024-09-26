a6cc3e3918811c9117e1dbf727db672bc845af9f
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2024-09-26 17:58:43 +0530

    updated something

--- misc/Template.cpp
+++ misc/Template.cpp
@@ -11,6 +11,10 @@ using namespace std;
 #define debug(...)
 #endif
 
+using u8 = uint8_t;
+using i64 = int64_t;
+using u64 = uint64_t;
+
 int32_t main()
 {
     ios_base::sync_with_stdio(false), cin.tie(NULL);
