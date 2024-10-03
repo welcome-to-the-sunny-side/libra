94f78e08d383d59c4715b0fed92e9bade2927e30
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2024-10-04 02:17:24 +0530

    updated something

--- string/Trie.cpp
+++ string/Trie.cpp
@@ -1,26 +1,29 @@
 class TrieNode
 {
 public: 
-    int leaf;
+    // int sub = 0;
+    int leaf = 0;
+
+    // const int C = 26;
+    // array<int, 26> next; 
     map<int, int> next;
-    TrieNode() : leaf(0) {};
 };
-template<typename S, typename T, typename C>
+template<typename S, typename T, typename I>
 class TrieChan
 {
 public:
     int r = 0, p = 0;
     vector<T> t;
-    C c;
+    I ind;
 
-    TrieChan(int n, C c) : t(n), c(c) {};
+    TrieChan(int n, I ind) : t(n), ind(ind) {};
 
     void Insert(const vector<S> &s)
     {
         int v = r;
         for(auto x : s)
         {
-            int f = c(x);
+            int f = ind(x);
         
             if(t[v].next[f] == 0)
                 t[v].next[f] = ++ p;
