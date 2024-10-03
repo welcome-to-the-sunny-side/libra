e727111320c8cdce0c4949478fdf8ee72f2c5dec
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2024-10-04 02:29:26 +0530

    updated something

---
+++ data/MonoidDeque.cpp
@@ -0,0 +1,17 @@
+template<typename T>
+class MonoidDequeChan
+{
+    stack<T> a, b;
+
+    void Push(T a)
+    {
+
+    }
+
+    void Pop()
+    {
+
+    }
+
+    void Compute
+};
--- numeric/Combinatorics.cpp
+++ numeric/Combinatorics.cpp
@@ -1,16 +1,15 @@
-template<typename T>
+template<typename T, const int P>
 class Combinatorics
 {
-    static constexpr int P = 33;
 public:
     int n;
     vector<T> inv, fac, ifac, pw;
     Combinatorics (int n) : n(n), inv(n), fac(n), ifac(n), pw(n)
     {
-        fac[0] = inv[0] = ifac[0] = pw[0] = 1;
+        fac[0] = inv[0] = ifac[0] = pw[0] = T(1);
  
         for(int i = 1; i <= n; i ++)
-            inv[i] = 1/(T)i, fac[i] = fac[i - 1] * i, ifac[i] = ifac[i - 1] * inv[i], pw[i] = pw[i - 1] * P;
+            inv[i] = T(1)/T(i), fac[i] = fac[i - 1] * T(i), ifac[i] = ifac[i - 1] * inv[i], pw[i] = pw[i - 1] * T(P);
     }
 
     T ncr(int n, int r)
