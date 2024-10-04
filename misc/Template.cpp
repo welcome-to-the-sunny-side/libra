9dd67b3108f3379ed0b4faeee7530f5f9ba31a12
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2024-10-04 16:16:24 +0530

    updated something

--- misc/Template.cpp
+++ misc/Template.cpp
@@ -4,14 +4,14 @@ using namespace std;
 #ifdef natural_selection
 #include "../libra/misc/dbg.h"
 #else
-#define endl "\n"
 #define debug(...)
+#define endl "\n"
 #endif
 
 int32_t main()
 {
     ios_base::sync_with_stdio(false), cin.tie(NULL);
-    int t;
+    int t = 1;
     cin >> t;
     while(t --)
     {
