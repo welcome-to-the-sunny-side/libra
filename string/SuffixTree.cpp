bb886a0a1df30c632c6b95bf13a378e293d4c331
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2024-10-30 21:41:51 +0530

    updated something

--- string/SuffixTree.cpp
+++ string/SuffixTree.cpp
@@ -12,10 +12,11 @@ class SuffixTreeChan
             2. for every suffix there exists node in the suffix tree (even if it has only child)
             3. if u > v, then u cannot be in the subtree of v
             4. adj[u] contains downward edges from node u, sorted lexicography in order of strings on their labels
-            5. root is root node
+            5. `root` is root node
             6. dep[u] contains length of path from root to node u, used for edge labels
-            7. rep[u] = some i such that node for suffix i is in subtree of u 
-        
+            7. rep[u] = some i such that leaf for suffix i is in subtree of u 
+            8. m is the total number of nodes in the suffix tree (so m - n is the number of new internal nodes)
+
         warning:
             sa gets modified so dont pass by reference
     */
@@ -25,7 +26,7 @@ public:
     vector<vector<int>> adj;
 
     SuffixTreeChan(int n, vector<int> sa, vector<int> lcp) : 
-    n(n), m(n - 1), dep(2 * n + 1), rep(2 * n + 1), adj(2 * n + 1)
+    n(n), m(n), dep(2 * n + 1), rep(2 * n + 1), adj(2 * n + 1)
     {
         vector<int> l(n), r(n);
         for(int i = 0; i < n; i ++) 
@@ -44,7 +45,7 @@ public:
                 else if(dep[sa[i + 1]] == d)
                     u = sa[i + 1], adj[u].push_back(sa[i]);
                 else
-                    u = ++ m, rep[u] = rep[sa[i]], adj[u] = {sa[i], sa[i + 1]};
+                    u = m ++, rep[u] = rep[sa[i]], adj[u] = {sa[i], sa[i + 1]};
                 
                 dep[u] = d;
                 l[r[i + 1]] = l[i], r[l[i]] = r[i + 1];
@@ -53,6 +54,6 @@ public:
 
         root = sa[0];
         if(dep[root] != 0)
-            root = ++ m, dep[root] = 0, adj[root] = {sa[0]};
+            root = m ++, dep[root] = 0, adj[root] = {sa[0]};
     };
 };
