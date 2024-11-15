4cce9aff404dc73e98cbdd477b487f90ad652e38
unalive <unalive@Shreyass-MacBook-Air.local>
2024-11-16 01:50:18 +0530

    updated something

--- data/bst/BSTNode.cpp
+++ data/bst/BSTNode.cpp
@@ -1,23 +1,14 @@
 class Node
 {
 public:
+//tree info
     Node *l, *r, *p;
-
     //self
     int key;
-
     //subtree aggregate
     int siz;
 
-    Node() : p(nullptr), l(nullptr), r(nullptr), key(0), siz(0) {};
-
-    virtual void Push() = 0;
-    virtual void Pull() = 0;
-};
-
-class SumNode : public Node
-{
-public:
+//monoid info
     //self
     int64_t val;
 
@@ -27,24 +18,25 @@ public:
     //lazy propagation
     int64_t add;
 
-    SumNode() : val(0), sum(0), add(0) {};
-
+    Node() : p(nullptr), l(nullptr), r(nullptr), key(0), siz(0),
+             val(0), sum(0), add(0) {};
+    
     void Push()
     {
         val += add;
         if(l != nullptr)
-            ((SumNode*)l)->sum += add * l->siz, ((SumNode*)l)->add += add;
+            l->sum += add * l->siz, l->add += add;
         if(r != nullptr)
-            ((SumNode*)r)->sum += add * r->siz, ((SumNode*)r)->add += add;
+            r->sum += add * r->siz, r->add += add;
         add = 0;
     }
     void Pull()
     {
         siz = sum = 0;
         if(l != nullptr)
-            siz += l->siz, sum += ((SumNode*)l)->sum;
+            siz += l->siz, sum += l->sum;
         siz += 1, sum += val;
         if(r != nullptr)
-            siz += r->siz, sum += ((SumNode*)r)->sum;
+            siz += r->siz, sum += r->sum;
     }
 };
