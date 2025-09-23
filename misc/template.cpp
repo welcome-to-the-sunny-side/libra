431060259884c1964b767353887a32683074a24a
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2025-09-23 19:20:00 +0530

    updated something

--- misc/template.cpp
+++ misc/template.cpp
@@ -1,4 +1,34 @@
-#include<bits/stdc++.h>
+#include <bitset>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <cstdio>
+#include <vector>
+#include <array>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <utility>
+#include <tuple>
+#include <cstdint>
+#include <limits>
+#include <type_traits>
+#include <cmath>
+#include <complex>
+#include <random>
+#include <chrono>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <cstdlib>
 using namespace std;
 
 #ifdef natural_selection
