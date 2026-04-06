1a5f7db628a1d633e07afc3f06bbc7e681b8312e
nonadhocproblems <nonadhocproblems@gmail.com>
2026-04-06 16:45:29 +0530

    multi mint

--- numeric/mod_int_multi.cpp
+++ numeric/mod_int_multi.cpp
@@ -1,82 +1,124 @@
 template <typename... Mints>
 struct multi_mint
 {
-    static constexpr int D = sizeof...(Mints);
-    tuple<Mints...> v;
+    static constexpr size_t D = sizeof...(Mints);
+    std::tuple<Mints...> v;
 
     multi_mint() = default;
+
     multi_mint(int64_t x) : v(Mints(x)...) {}
 
-    template <typename... Ts, typename = enable_if_t<sizeof...(Ts) == D>>
+    template <typename... Ts, typename = std::enable_if_t<sizeof...(Ts) == D>>
     multi_mint(Ts... xs) : v(Mints(xs)...) {}
 
 private:
     template <size_t... I>
-    void add_impl(const multi_mint &other, index_sequence<I...>)
+    void add_impl(const multi_mint &other, std::index_sequence<I...>)
     {
         ((std::get<I>(v) += std::get<I>(other.v)), ...);
     }
 
     template <size_t... I>
-    void sub_impl(const multi_mint &other, index_sequence<I...>)
+    void sub_impl(const multi_mint &other, std::index_sequence<I...>)
     {
         ((std::get<I>(v) -= std::get<I>(other.v)), ...);
     }
 
     template <size_t... I>
-    void mul_impl(const multi_mint &other, index_sequence<I...>)
+    void mul_impl(const multi_mint &other, std::index_sequence<I...>)
     {
         ((std::get<I>(v) *= std::get<I>(other.v)), ...);
     }
 
     template <size_t... I>
-    void div_impl(const multi_mint &other, index_sequence<I...>)
+    void div_impl(const multi_mint &other, std::index_sequence<I...>)
     {
         ((std::get<I>(v) /= std::get<I>(other.v)), ...);
     }
 
     template <size_t... I>
-    bool eq_impl(const multi_mint &other, index_sequence<I...>) const
+    bool eq_impl(const multi_mint &other, std::index_sequence<I...>) const
     {
         return ((std::get<I>(v) == std::get<I>(other.v)) && ...);
     }
 
     template <size_t... I>
-    multi_mint neg_impl(index_sequence<I...>) const
+    bool lt_impl(const multi_mint &other, std::index_sequence<I...>) const
+    {
+        return v < other.v; // tuple lexicographic comparison
+    }
+
+    template <size_t... I>
+    multi_mint neg_impl(std::index_sequence<I...>) const
     {
         multi_mint res;
         res.v = std::make_tuple((-std::get<I>(v))...);
         return res;
     }
 
+    template <size_t... I>
+    multi_mint inv_impl(std::index_sequence<I...>) const
+    {
+        multi_mint res;
+        res.v = std::make_tuple(std::get<I>(v).inv()...);
+        return res;
+    }
+
+    template <size_t... I>
+    multi_mint pow_impl(int64_t p, std::index_sequence<I...>) const
+    {
+        multi_mint res;
+        res.v = std::make_tuple(std::get<I>(v).pow(p)...);
+        return res;
+    }
+
+    template <size_t... I>
+    void print_impl(std::ostream &os, std::index_sequence<I...>) const
+    {
+        os << '{';
+        bool first = true;
+        ((os << (std::exchange(first, false) ? "" : ", ") << std::get<I>(v)), ...);
+        os << '}';
+    }
+
 public:
     multi_mint &operator+=(const multi_mint &other)
     {
-        add_impl(other, make_index_sequence<D>{});
+        add_impl(other, std::make_index_sequence<D>{});
         return *this;
     }
 
     multi_mint &operator-=(const multi_mint &other)
     {
-        sub_impl(other, make_index_sequence<D>{});
+        sub_impl(other, std::make_index_sequence<D>{});
         return *this;
     }
 
     multi_mint &operator*=(const multi_mint &other)
     {
-        mul_impl(other, make_index_sequence<D>{});
+        mul_impl(other, std::make_index_sequence<D>{});
         return *this;
     }
 
     multi_mint &operator/=(const multi_mint &other)
     {
-        div_impl(other, make_index_sequence<D>{});
+        div_impl(other, std::make_index_sequence<D>{});
         return *this;
     }
 
     multi_mint operator-() const
     {
-        return neg_impl(make_index_sequence<D>{});
+        return neg_impl(std::make_index_sequence<D>{});
+    }
+
+    multi_mint inv() const
+    {
+        return inv_impl(std::make_index_sequence<D>{});
+    }
+
+    multi_mint pow(int64_t p) const
+    {
+        return pow_impl(p, std::make_index_sequence<D>{});
     }
 
     friend multi_mint operator+(multi_mint a, const multi_mint &b) { return a += b; }
@@ -86,15 +128,46 @@ public:
 
     friend bool operator==(const multi_mint &a, const multi_mint &b)
     {
-        return a.eq_impl(b, make_index_sequence<D>{});
+        return a.eq_impl(b, std::make_index_sequence<D>{});
     }
 
     friend bool operator!=(const multi_mint &a, const multi_mint &b)
     {
         return !(a == b);
     }
-};
 
+    friend bool operator<(const multi_mint &a, const multi_mint &b)
+    {
+        return a.v < b.v;
+    }
+
+    friend bool operator>(const multi_mint &a, const multi_mint &b)
+    {
+        return b < a;
+    }
+
+    friend bool operator<=(const multi_mint &a, const multi_mint &b)
+    {
+        return !(b < a);
+    }
+
+    friend bool operator>=(const multi_mint &a, const multi_mint &b)
+    {
+        return !(a < b);
+    }
+
+    template <size_t I>
+    auto &get() { return std::get<I>(v); }
+
+    template <size_t I>
+    const auto &get() const { return std::get<I>(v); }
+
+    friend std::ostream &operator<<(std::ostream &os, const multi_mint &m)
+    {
+        m.print_impl(os, std::make_index_sequence<D>{});
+        return os;
+    }
+};
 template <const int &...MODS>
 using multi_mint_mods = multi_mint<modular_int<MODS>...>;
 
