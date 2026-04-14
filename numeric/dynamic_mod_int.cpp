1c98e0c60deb0665244680d450592368a4d506cb
nonadhocproblems <nonadhocproblems@gmail.com>
2026-04-15 00:20:41 +0530

    added dynamic mint and arbitrary dynamic mod poly mult

---
+++ numeric/dynamic_mod_int.cpp
@@ -0,0 +1,165 @@
+template<int ID = 0>
+struct dynamic_modular_int
+{
+    inline static int MOD = 998244353;
+
+    static void set_mod(int m)
+    {
+        assert(m > 0);
+        MOD = m;
+    }
+    
+    int val;
+
+    dynamic_modular_int(int64_t v = 0)
+    {
+        if (v < 0)
+            v = v % MOD + MOD;
+        if (v >= MOD)
+            v %= MOD;
+        val = int(v);
+    }
+
+    dynamic_modular_int(uint64_t v)
+    {
+        if (v >= MOD)
+            v %= MOD;
+        val = int(v);
+    }
+
+    dynamic_modular_int(int v) : dynamic_modular_int(int64_t(v)) {}
+    dynamic_modular_int(unsigned v) : dynamic_modular_int(uint64_t(v)) {}
+
+    explicit operator int() const { return val; }
+    explicit operator unsigned() const { return val; }
+    explicit operator int64_t() const { return val; }
+    explicit operator uint64_t() const { return val; }
+    explicit operator double() const { return val; }
+    explicit operator long double() const { return val; }
+
+    dynamic_modular_int &operator+=(const dynamic_modular_int &other)
+    {
+        val -= MOD - other.val;
+        if (val < 0)
+            val += MOD;
+        return *this;
+    }
+
+    dynamic_modular_int &operator-=(const dynamic_modular_int &other)
+    {
+        val -= other.val;
+        if (val < 0)
+            val += MOD;
+        return *this;
+    }
+
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD)
+    {
+#if !defined(_WIN32) || defined(_WIN64)
+        return unsigned(x % m);
+#endif
+        // Optimized mod for Codeforces 32-bit machines.
+        // x must be less than 2^32 * m for this to work, so that x / m fits in an unsigned 32-bit int.
+        unsigned x_high = unsigned(x >> 32), x_low = unsigned(x);
+        unsigned quot, rem;
+        asm("divl %4\n"
+            : "=a"(quot), "=d"(rem)
+            : "d"(x_high), "a"(x_low), "r"(m));
+        return rem;
+    }
+
+    dynamic_modular_int &operator*=(const dynamic_modular_int &other)
+    {
+        val = fast_mod(uint64_t(val) * other.val);
+        return *this;
+    }
+
+    dynamic_modular_int &operator/=(const dynamic_modular_int &other)
+    {
+        return *this *= other.inv();
+    }
+
+    friend dynamic_modular_int operator+(const dynamic_modular_int &a, const dynamic_modular_int &b) { return dynamic_modular_int(a) += b; }
+    friend dynamic_modular_int operator-(const dynamic_modular_int &a, const dynamic_modular_int &b) { return dynamic_modular_int(a) -= b; }
+    friend dynamic_modular_int operator*(const dynamic_modular_int &a, const dynamic_modular_int &b) { return dynamic_modular_int(a) *= b; }
+    friend dynamic_modular_int operator/(const dynamic_modular_int &a, const dynamic_modular_int &b) { return dynamic_modular_int(a) /= b; }
+
+    dynamic_modular_int &operator++()
+    {
+        val = val == MOD - 1 ? 0 : val + 1;
+        return *this;
+    }
+
+    dynamic_modular_int &operator--()
+    {
+        val = val == 0 ? MOD - 1 : val - 1;
+        return *this;
+    }
+
+    dynamic_modular_int operator++(int)
+    {
+        dynamic_modular_int before = *this;
+        ++*this;
+        return before;
+    }
+    dynamic_modular_int operator--(int)
+    {
+        dynamic_modular_int before = *this;
+        --*this;
+        return before;
+    }
+
+    dynamic_modular_int operator-() const
+    {
+        return val == 0 ? 0 : MOD - val;
+    }
+
+    friend bool operator==(const dynamic_modular_int &a, const dynamic_modular_int &b) { return a.val == b.val; }
+    friend bool operator!=(const dynamic_modular_int &a, const dynamic_modular_int &b) { return a.val != b.val; }
+    friend bool operator<(const dynamic_modular_int &a, const dynamic_modular_int &b) { return a.val < b.val; }
+    friend bool operator>(const dynamic_modular_int &a, const dynamic_modular_int &b) { return a.val > b.val; }
+    friend bool operator<=(const dynamic_modular_int &a, const dynamic_modular_int &b) { return a.val <= b.val; }
+    friend bool operator>=(const dynamic_modular_int &a, const dynamic_modular_int &b) { return a.val >= b.val; }
+
+    dynamic_modular_int inv() const
+    {
+        dynamic_modular_int product = 1;
+        int v = val;
+
+        do
+        {
+            product *= MOD - MOD / v;
+            v = MOD % v;
+        } while (v > 1);
+
+        return product;
+    }
+
+    dynamic_modular_int pow(int64_t p) const
+    {
+        if (p < 0)
+            return inv().pow(-p);
+
+        dynamic_modular_int a = *this, result = 1;
+
+        while (p > 0)
+        {
+            if (p & 1)
+                result *= a;
+
+            p >>= 1;
+
+            if (p > 0)
+                a *= a;
+        }
+
+        return result;
+    }
+
+    friend ostream &operator<<(ostream &os, const dynamic_modular_int &m)
+    {
+        return os << m.val;
+    }
+};
+using dmint = dynamic_modular_int<0>;
+void __print(dmint x) { cerr << x; }
---
+++ numeric/poly_arbitrary_dynamic.cpp
@@ -0,0 +1,67 @@
+template <int ID = 0>
+struct dynamic_arbitrary_poly
+{
+    using dmint = dynamic_modular_int<ID>;
+
+    static constexpr int P1 = 998244353;
+    static constexpr int P2 = 1004535809;
+    static constexpr int P3 = 469762049;
+
+    using NTT1 = ntt_poly<P1, 3>;
+    using NTT2 = ntt_poly<P2, 3>;
+    using NTT3 = ntt_poly<P3, 3>;
+
+    using m1 = modular_int<P1>;
+    using m2 = modular_int<P2>;
+    using m3 = modular_int<P3>;
+
+    static dmint garner3(int x1, int x2, int x3)
+    {
+        static const m2 inv_p1_mod_p2 = m2(P1).inv();
+        static const m3 inv_p1p2_mod_p3 = (m3(P1) * m3(P2)).inv();
+
+        int t1 = x1;
+        int t2 = ((m2(x2) - m2(t1)) * inv_p1_mod_p2).val;
+        int t3 = ((m3(x3) - m3(t1) - m3(P1) * m3(t2)) * inv_p1p2_mod_p3).val;
+
+        dmint res = dmint(t1);
+        res += dmint(P1) * dmint(t2);
+        res += dmint(1LL * P1 * P2) * dmint(t3);
+        return res;
+    }
+    static vector<dmint> multiply(const vector<dmint> &a, const vector<dmint> &b)
+    {
+        if (a.empty() || b.empty())
+            return {};
+
+        vector<m1> a1(a.size()), b1(b.size());
+        vector<m2> a2(a.size()), b2(b.size());
+        vector<m3> a3(a.size()), b3(b.size());
+
+        for (int i = 0; i < (int)a.size(); i++)
+        {
+            int x = a[i].val;
+            a1[i] = x;
+            a2[i] = x;
+            a3[i] = x;
+        }
+        for (int i = 0; i < (int)b.size(); i++)
+        {
+            int x = b[i].val;
+            b1[i] = x;
+            b2[i] = x;
+            b3[i] = x;
+        }
+
+        auto c1 = NTT1::multiply(a1, b1);
+        auto c2 = NTT2::multiply(a2, b2);
+        auto c3 = NTT3::multiply(a3, b3);
+
+        int n = (int)c1.size();
+        vector<dmint> res(n);
+        for (int i = 0; i < n; i++)
+            res[i] = garner3(c1[i].val, c2[i].val, c3[i].val);
+
+        return res;
+    }
+};
